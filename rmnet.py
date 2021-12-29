d5e55ddd2dd7d77fc42da3b7840b39fa158e5746
Jireh Jam <16281283+Jireh-Jam@users.noreply.github.com>
2021-12-29 13:46:28 +0000

    Update rmnet.py

--- rmnet.py
+++ rmnet.py
@@ -27,6 +27,7 @@ import numpy as np
 
 import os
 import datetime
+import time
 import gc
 
    
@@ -42,6 +43,8 @@ class RMNETWGAN():
         self.missing_shape = (self.img_width, self.img_height, self.channels)
         self.num_epochs = config.num_epochs
         self.batch_size = config.batch_size
+        self.start_time = time.time()
+        self.end_time = time.time() 
         self.sample_interval = config.sample_interval
         self.current_epoch =config.current_epoch
         self.last_trained_epoch = config.last_trained_epoch
@@ -51,8 +54,10 @@ class RMNETWGAN():
         self.models_path = 'models'
         
         #Configure Loader
-        self.train_img_dir ='./images/train/'
-        self.train_mask_dir ='./masks/train/'
+        self.img_dir = r'./images/train/celebA_HQ_train/'
+        self.masks_dir = r'./masks/train/qd_imd/train/'
+        self.imgs_in_path = os.listdir(self.img_dir)
+        self.masks_in_path = os.listdir(self.masks_dir) 
         
         # Number of filters in the first layer of G and D
         self.gf = config.gf
@@ -253,126 +258,103 @@ class RMNETWGAN():
         return model       
 
     # =================================================================================== #
-    #               8. Define the train function                                          #
+    #               8. Define the loading function                                        #
     # =================================================================================== #  
-        
+    def get_batch(self, imgs_index, batch_imgs):
+        if(imgs_index+batch_imgs) >= len(self.imgs_in_path):
+            batch_imgs = len(self.imgs_in_path)-imgs_index
+        real_imgs = np.zeros((batch_imgs, self.img_width, self.img_height,3))
+        masks = np.zeros((batch_imgs, self.img_width, self.img_height,1))
+        masked_imgs = np.zeros((batch_imgs, self.img_width, self.img_height,3))
+        masks_index = random.sample(range(1,len(self.masks_in_path)), batch_imgs)
+        maskindx = 0
+        for i in range(batch_imgs):
+            print("\rLoading image number "+ str(i) + " of " + str(len(self.imgs_in_path)), end = " ")
+            real_img = cv2.imread(self.img_dir + self.imgs_in_path[imgs_index], 1).astype('float')/ 127.5 -1
+            real_img = cv2.resize(real_img,(self.img_width, self.img_height))
+            mask = 1-cv2.imread(self.masks_dir + self.masks_in_path[masks_index[maskindx]],0).astype('float')/ 255
+            mask = cv2.resize(mask,(self.img_width, self.img_height))
+            mask = np.reshape(mask,(self.img_width, self.img_height,1))
+
+            masks[i] = mask
+            real_imgs[i] = real_img
+            #masked_imgs[np.where((mask ==[1,1,1]).all(axis=2))]=[255,255,255]
+            masked_imgs[i][np.where(mask == 0)]=1
+            maskindx +=1
+            imgs_index +=1
+            if(imgs_index >= len(self.imgs_in_path)):
+                imgs_index = 0
+#            cv2.imwrite(os.path.join(path, 'mask_'+str(i)+'.jpg'),rawmask)
+#            cv2.imshow("mask",((masked_imgs[0]+1)* 127.5).astype("uint8"))
+#            cv2.waitKey(0 )
+        return imgs_index,real_imgs, masks,masked_imgs 
+    # =================================================================================== #
+    #               8. Define the loading function                                        #
+    # =================================================================================== #
     def train(self):
-            
         # Ground truths for adversarial loss
         valid = np.ones([self.batch_size, 1])
         fake = np.zeros((self.batch_size, 1))
-        
-        #Create arrays to store losses
-        avg_d_loss=[]
-        avg_g_loss=[]
-        
-        #Load data  
-        preprocess_images = lambda x: (x - 127.5) / 127.5
-        img_generator = ImageDataGenerator(rotation_range=15,horizontal_flip=True,preprocessing_function=preprocess_images)
-        img_gen = img_generator.flow_from_directory(self.train_img_dir, 
-                                                    target_size=(self.img_width, self.img_height), 
-                                                    class_mode=None,
-                                                    batch_size=self.batch_size, 
-                                                    shuffle=True,  
-                                                    interpolation="nearest", 
-                                                    seed=123)
-        
-        preprocess_mask = lambda x: x * 1. / 255
-        mask_generator = ImageDataGenerator(preprocessing_function=preprocess_mask)
-        mask_gen = mask_generator.flow_from_directory(self.train_mask_dir, 
-                                                      target_size=(self.img_width, self.img_height), 
-                                                      class_mode=None,
-                                                      batch_size=self.batch_size, 
-                                                      color_mode="grayscale", 
-                                                      shuffle=True,  
-                                                      interpolation="nearest", 
-                                                      seed=123)
-        
-        gc.collect() 
-        num_samples = img_gen.samples
-        wgan_num_steps = int(num_samples//self.batch_size)
-        print("Number of WGAN steps {}".format(wgan_num_steps))
-        global_step = 0
-        
-        discriminator_loss = []
-        generator_loss = []
-        
-        init = tf.global_variables_initializer()
-        with tf.device("gpu:0"):
-            print("tf.keras code in this scope will run on GPU")        
-            config = tf.ConfigProto()
-            config.gpu_options.allow_growth = True
-            sess = tf.Session(config=config)
-            os.environ['CUDA_VISIBLE_DEVICES'] = "0" 
-            sess.run(init)
-#         if self.continue_train:
-#             #Edit last_trained_epoch in config.ini
-#             self.generator.load_weights(r'../{}/{}/weight_{}.h5'.format(self.models_path, self.dataset_name, self.last_trained_epoch))
-#             print ( "Successfully loaded last check point" )
-#         else:
-#             print ( "Failed to get check point" )                
-        
-        for epoch in range(self.num_epochs):
-            
-          batch_start_time = datetime.datetime.now() 
-          step = 0
-          for real_img in img_gen:
-              mask = next(mask_gen)
-              if step == wgan_num_steps:
-                  break
-              step +=1
+        total_files= 27000
+        batch_imgs = 1000
+        imgs_index =0
+        dataLoads = total_files//batch_imgs
+        #self.generator.load_weights(r'./{}/{}/weight_{}.h5'.format(self.models_path, self.dataset_name, self.last_trained_epoch))
+        # print ( "Successfully loaded last check point" )
+        for epoch in range(1, self.num_epochs + 1):
 
-    # =================================================================================== #
-    #                             8.1. Predict images during training                     #
-    # =================================================================================== #   
-    
-              gen_imgs=self.generator.predict([real_img,mask], self.batch_size) 
-              gen_imgs = gen_imgs[:,:,:,0:3]        
-              
-    # =================================================================================== #
-    #                             8.2. Train the discriminator                            #
-    # =================================================================================== # 
-    
-              d_loss_real = self.discriminator.train_on_batch(real_img, valid)
-              d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
-              d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-              discriminator_loss.append(d_loss)
-              avg_d_loss.append(np.mean(discriminator_loss))
-    
-    # =================================================================================== #
-    #                             8.3. Train the generator                                #
-    # =================================================================================== #
-     
-              g_loss = self.combined.train_on_batch([real_img ,mask], [real_img, valid])
-              generator_loss.append(g_loss)
-              avg_g_loss.append(np.mean(generator_loss))
-              elapsed_time = datetime.datetime.now() - batch_start_time    
-                 
-    # =================================================================================== #
-    #                             8.4. Plot the progress                                  #
-    # =================================================================================== #
-    
-              gc.collect()   
-              if global_step % self.sample_interval == 0:  
-                input_img = np.expand_dims(real_img[0], 0)
-                input_mask = np.expand_dims(mask[0], 0)
+            for databatchs in range(dataLoads):
+                imgs_index,imgs, masks,masked_imgs = self.get_batch(imgs_index, batch_imgs)
+                batches = imgs.shape[0]//self.batch_size
+                global_step = 0
+                for batch in range(batches):
+                    idx = np.random.permutation(imgs.shape[0])
+                    idx_batches = idx[batch*self.batch_size:(batch+1)*self.batch_size]
+                    gen_imgs=self.generator.predict([imgs[idx_batches],masks[idx_batches]], self.batch_size)
+                    gen_imgs = gen_imgs[:,:,:,0:3]
+
+                    # =================================================================================== #
+                    #                             8.2. Train the discriminator                            #
+                    # =================================================================================== # 
+                    d_loss_real = self.discriminator.train_on_batch(imgs[idx_batches], valid)
+                    d_loss_fake = self.discriminator.train_on_batch(gen_imgs[:,:,:,0:3], fake)
+                    d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+
+                    # =================================================================================== #
+                    #                             8.3. Train the generator                                #
+                    # =================================================================================== #
+
+                    # Train the generator
+                    g_loss = self.combined.train_on_batch([imgs[idx_batches] ,masks[idx_batches]], [imgs[idx_batches],valid])
+
+                    # =================================================================================== #
+                    #                             8.4. Plot the progress                                  #
+                    # =================================================================================== #
+                    print ("Epoch: %d Batch: %d/%d dataloads: %d/%d [D loss: %f, op_acc: %.2f%%]  [G loss: %f MSE loss: %f]" % (epoch+self.current_epoch,
+                               batch, batches,databatchs,dataLoads, d_loss[0], 100*d_loss[1], g_loss[0], g_loss[1]))
+
+
+            idx_batches = idx[databatchs*self.batch_size:(databatchs+1)*self.batch_size]
+            imgs = imgs[idx]
+            masks = masks[idx]
+
+            input_img = np.expand_dims(imgs[0], 0)
+            input_mask = np.expand_dims(masks[0], 0)
+
+            if epoch % 1 == 0:
                 if not os.path.exists("{}/{}/".format(self.models_path, self.dataset_name)):
-                    os.makedirs("{}/{}/".format(self.models_path, self.dataset_name))
-                print("[Epoch %d/%d] [Global Steps: %d/%d] [WGAN Steps: %d ] [D loss: %f, acc: %3d%%] [G loss: %05f, adv: %05f] time: %s " \
-                          % (epoch, self.num_epochs,step,global_step,wgan_num_steps,
-                             d_loss[0], 100 * d_loss[1],
-                             g_loss[0],
-                             np.mean(g_loss[1:3]),
-                             elapsed_time))     
-                print("Seen so far: %s samples" % ((global_step + 1) * self.batch_size)) 
+                     os.makedirs("{}/{}/".format(self.models_path, self.dataset_name))
                 name = "{}/{}/weight_{}.h5".format(self.models_path, self.dataset_name, epoch+self.current_epoch)
+
                 self.generator.save_weights(name)
                 if not os.path.exists(self.dataset_name):
                     os.makedirs(self.dataset_name,exist_ok=True)
                 predicted_img = self.generator.predict([input_img, input_mask])
                 self.sample_images(self.dataset_name, input_img, predicted_img[:,:,:,0:3],
                                         input_mask, epoch)
-              global_step += 1
+        print("Total Processing time:: {:4.2f}min" .format((self.end_time - self.start_time)/60))
+        self.epoch+=1
 
     # =================================================================================== #
     #               9. Sample images during training                                      #
@@ -384,9 +366,9 @@ class RMNETWGAN():
         input_img = np.expand_dims(input_img[0], 0)
         input_mask = np.expand_dims(mask[0], 0)
         maskedImg = ((1 - input_mask)*input_img) + input_mask       
-        img = np.concatenate(((maskedImg[0][:, :, ::-1]* 127.5 + 127.5).astype(np.uint8),
-                              (sample_pred[0][:, :, ::-1]* 127.5 + 127.5).astype(np.uint8),
-                              (input_img[0][:, :, ::-1]* 127.5 + 127.5).astype(np.uint8)),axis=1)
+        img = np.concatenate((((maskedImg[0]+1)* 127.5).astype("uint8"),
+                              ((sample_pred[0]+1)* 127.5).astype("uint8"),
+                              ((input_img[0]+1)* 127.5).astype("uint8")),axis=1)
         img_filepath = os.path.join(self.dataset_name, 'pred_{}.jpg'.format(epoch+self.current_epoch))
 
         cv2.imwrite(img_filepath, img) 
@@ -408,6 +390,3 @@ class RMNETWGAN():
         plt.ylabel('Loss (Adversarial/Discriminator)')
         plt.legend()
         plt.savefig("LogsUnet/{}_paper/log_ep{}.pdf".format(self.dataset_name, epoch+self.current_epoch))
-
-
-          
